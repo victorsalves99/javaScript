9029521e681a5823284e5084fc46efc004efaed1
VICTOR SANTANA ALVES <96845724+victorsalves99@users.noreply.github.com>
2022-08-15 19:51:41 -0300

    1.5

---
+++ aulas/ex.015.js
@@ -0,0 +1,8 @@
+let num = [5,8,2]
+num.push(0)
+num.length
+
+console.log(`nossos vetores sao :${num}`)
+console.log(`nossos vetores tem  :${num.length} posições`)
+console.log(`nossos vetores em ordem sao :${num.sort()}`)
+
