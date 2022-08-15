eb93a1c13647263bb08713081271ed01046cf055
VICTOR SANTANA ALVES <96845724+victorsalves99@users.noreply.github.com>
2022-08-15 19:09:20 -0300

    finalizado

--- desafios/projeto4/main.js
+++ desafios/projeto4/main.js
@@ -1,12 +1,17 @@
 function gerar(){
-    let num = document.querySelector('#num')
-    let n = num.value
-    console.log(n)
-    let tab = document.querySelector('#taboada')
-    tab.innerHTML=''
-    for (var c=0 ; c<=10 ; c++){
-        let item = document.createElement('option')
-        item.text=`${n} x ${c} = ${n*c}`
-        tab.appendChild(item)
+    if (num.value==0){
+        alert('campo vazio')
+    }else{
+        let num = document.querySelector('#num')
+        let n = num.value
+        console.log(n)
+        let tab = document.querySelector('#taboada')
+        tab.innerHTML=''
+        for (var c=0 ; c<=10 ; c++){
+            let item = document.createElement('option')
+            item.text=`${n} x ${c} = ${n*c}`
+            item.value=`tab${c}`
+            tab.appendChild(item)
+        }
     }
 }
